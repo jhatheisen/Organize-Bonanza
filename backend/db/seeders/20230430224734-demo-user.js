249d4ff0b48097da57e6d3e4ca7b23407f7bb4d6
Jhass Theisen <jhasstheisen1@gmail.com>
2023-04-30 15:49:21 -0700

    modeled, migrated, seeded

---
+++ backend/db/seeders/20230430224734-demo-user.js
@@ -0,0 +1,38 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [
+      {
+        email: 'user1@user.io',
+        username: 'DemoUser1',
+        hashedPassword: bcrypt.hashSync('password1')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'DemoUser2',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user3@user.io',
+        username: 'DemoUser3',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['DemoUser1', 'DemoUser2', 'DemoUser3'] }
+    }, {});
+  }
+};
