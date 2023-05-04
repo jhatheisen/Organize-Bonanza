3418123e17e8e7866405995224928fc5ff414042
Jhass Theisen <jhasstheisen1@gmail.com>
2023-05-04 12:19:55 -0700

    nav

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -8,6 +8,7 @@ import SignupFormModal from '../SignupFormModal';
 import * as sessionActions from '../../store/session';
 import './Navigation.css';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import logo from './logo.png';
 
 function Navigation({ isLoaded }){
 
@@ -54,13 +55,15 @@ function Navigation({ isLoaded }){
 
   return (
     <ul className='flex justify-between bg-zinc-700'>
+      <div className='p-3'>
+        <img src={logo} className='h-11'></img>
+      </div>
       { sessionUser && (
         <li className='mr-6 flex'><NavLink exact to='/orders' className='p-3 text-xl text-blue-500 hover:text-blue-300'>Orders</NavLink></li>
         )}
       <li className='mr-6 flex'>
-        { sessionUser ?
-          <NavLink exact to="/products" className='p-3 text-xl text-blue-500 hover:text-blue-300'>Products</NavLink> :
-          <NavLink exact to="/splash" className='p-3 text-xl text-blue-500 hover:text-blue-300'>Products</NavLink>
+        { sessionUser &&
+          <NavLink exact to="/products" className='p-3 text-xl text-blue-500 hover:text-blue-300'>Products</NavLink>
         }
       </li>
       {isLoaded && sessionLinks}
